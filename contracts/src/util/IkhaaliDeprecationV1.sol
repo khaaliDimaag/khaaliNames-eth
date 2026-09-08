499da8655b4298a0756b8aa1032f3b46b12530ca
theprimefibber <dev@thisispalash.com>
2026-09-09 02:58:19 +0530

    deprecation util completed!

--- contracts/src/util/IkhaaliDeprecationV1.sol
+++ contracts/src/util/IkhaaliDeprecationV1.sol
@@ -7,35 +7,64 @@ struct Version {
   uint128 patch;
 }
 
-
 /// @title IkhaaliDeprecation
 /// @notice Small module to handle contract deprecations
+/// @dev Consider supporting ERC-165 in V2
 interface IkhaaliDeprecationV1 {
+
+  enum UpdateKind { UNCHANGED, BREAKING, FEATURE, FIX }
 
   ////////// Errors //////////
 
+  error ContractAbandoned();
   error ContractDeprecated(address newAddress);
+
+  error NewVersionNotAContract();
+  error NewVersionMustNotBeZero();
+  error NewVersionMustBeGreater(string oldVersion, string newVersion);
 
   error NewAdminMustNotBeZero();
   error NewAdminMustBeNew(address admin);
 
   error AdminFunctionCalledByNonAdmin(address caller);
 
+
   ////////// Events //////////
 
-  event AdminUpdated(address oldAdmin, address newAdmin);
-  event Deprecated(address newVersion, Version oldVersion);
+  event Deprecated(
+    address indexed newAddress,
+    UpdateKind indexed kind,
+    Version oldVersion, Version newVersion
+  );
+  event Abandoned(Version current);
+  event AdminUpdated(address indexed oldAdmin, address indexed newAdmin);
 
-  ////////// Main Functions //////////
 
+  ////////// Public Functions //////////
+
+  /// @notice Get the current version of the contract as a string
   function version() external view returns (string memory);
+
+  /// @notice Get the current version of the contract as a tuple
+  function semantic() external view returns (Version memory);
+
+
+  ////////// Main Functionality //////////
 
   /// @notice Deprecate the current contract
   /// @param _new Address for the new version
   function deprecate(address _new) external;
 
-  ////////// Admin Functions //////////
+  /// @notice Remove support for current logic
+  /// @dev Consider supporting "fallbacks" in V2
+  function abandon() external;
 
+
+  ////////// Admin Related Functions //////////
+
+  /// @notice Update the admin manager of this contract
+  /// @dev V2 needs to explicitly check for EOA vs contract admin
+  /// @param _new Address for the new admin
   function updateAdmin(address _new) external;
 
 }
--- contracts/src/util/khaaliDeprecationV1.sol
+++ contracts/src/util/khaaliDeprecationV1.sol
@@ -3,33 +3,50 @@ pragma solidity ^0.8.20;
 
 import {IkhaaliDeprecationV1, Version} from "./IkhaaliDeprecationV1.sol";
 
-contract khaaliDeprecationV1 is IkhaaliDeprecationV1 {
+/// @dev Marked `abstract` since this is not meant to be standalone
+abstract contract khaaliDeprecationV1 is IkhaaliDeprecationV1 {
 
   Version _v;
   address public admin;
+  bool public isAbandoned;
   bool public isDeprecated;
   address public nextVersion;
 
+  ////////// Modifiers //////////
+
   modifier onlyAdmin() {
-    require(msg.sender == admin, AdminFunctionCalledByNonAdmin(msg.sender));
+    _onlyAdmin();
     _;
+  }
+
+  function _onlyAdmin() internal view {
+    require(msg.sender == admin, AdminFunctionCalledByNonAdmin(msg.sender));
   }
 
   modifier undeprecated() {
-    require(!isDeprecated, ContractDeprecated(nextVersion));
+    _undeprecated();
     _;
   }
 
+  function _undeprecated() internal view {
+    require(!isDeprecated, ContractDeprecated(nextVersion));
+    require(!isAbandoned, ContractAbandoned());
+  }
+
+  ////////// Initializers //////////
+
   constructor (uint64 _major, uint64 _minor, uint128 _patch) {
-    _v = Version(_major, _minor, _patch);
+    _v = Version({major: _major, minor: _minor, patch: _patch});
     admin = msg.sender; /// @dev in V2 let's be smarter about this
     isDeprecated = false;
     nextVersion = address(0);
   }
 
+  ////////// Public Functions //////////
+
   function version() public view returns (string memory) {
     return string(
-      abi.encodePacked(
+      string.concat(
         _uint2str(_v.major),
         ".",
         _uint2str(_v.minor),
@@ -39,25 +56,64 @@ contract khaaliDeprecationV1 is IkhaaliDeprecationV1 {
     );
   }
 
-  function deprecate(address _new) external onlyAdmin {
-
-
+  function semantic() public view returns (Version memory) {
+    return _v;
   }
+
+  ////////// Main Functionality //////////
+
+  function deprecate(address _new) external undeprecated onlyAdmin {
+    require(_new != address(0), NewVersionMustNotBeZero()); /// @dev cosmetic
+    require(_new.code.length > 0, NewVersionNotAContract());
+
+    Version memory _nv = IkhaaliDeprecationV1(_new).semantic();
+    bool ok = _nv.major > _v.major
+    || (_nv.major == _v.major && _nv.minor > _v.minor)
+    || (_nv.major == _v.major && _nv.minor == _v.minor && _nv.patch > _v.patch);
+    require(
+      ok,
+      NewVersionMustBeGreater(version(), IkhaaliDeprecationV1(_new).version())
+    );
+
+    UpdateKind _kind = _nv.major != _v.major ? UpdateKind.BREAKING
+      : _nv.minor != _v.minor ? UpdateKind.FEATURE : UpdateKind.FIX;
+
+    nextVersion = _new;
+    isDeprecated = true;
+
+    // reentrancy (should) not possible since only view functions called above
+    // forge-lint: disable-next-line(reentrancy-events)
+    emit Deprecated(_new, _kind, _v, _nv);
+  }
+
+  function abandon() external undeprecated onlyAdmin {
+    isAbandoned = true;
+    nextVersion = address(0); /// @dev extra sure to reset pointer; may not need
+    emit Abandoned(_v);
+  }
+
+  ////////// Admin Related Functions //////////
+
 
   function updateAdmin(address _new) external onlyAdmin {
     require(_new != admin, NewAdminMustBeNew(admin));
     require(_new != address(0), NewAdminMustNotBeZero());
 
+    address _old = admin; /// @dev only for the event; rather emit after update
     admin = _new;
+    emit AdminUpdated(_old, _new);
   }
 
+  ////////// Internal Functions //////////
+
+  /// @notice Convert a uint256 to its decimal string representation
   function _uint2str(uint256 _n) internal pure returns (string memory) {
 
     if(_n == 0) return "0";
 
     /// @dev count digits
     uint256 t = _n;
-    uint256 len;
+    uint256 len = 0;
     while(t != 0) {
       len++;
       t/=10;
@@ -67,10 +123,13 @@ contract khaaliDeprecationV1 is IkhaaliDeprecationV1 {
     bytes memory _str = new bytes(len);
     while(_n!=0) {
       len--; /// @dev reduce first since arrays start from 0
+      // casting to `uint8` is safe since the `% 10` ensures < 0x3A
+      // forge-lint: disable-next-line(unsafe-typecast)
       _str[len] = bytes1(uint8(0x30 + (_n % 10))); /// @dev 0x30 == "0"
       _n/=10;
     }
 
     return string(_str);
   }
+
 }
