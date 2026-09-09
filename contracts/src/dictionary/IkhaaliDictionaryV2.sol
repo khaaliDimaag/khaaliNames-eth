9f46abdb9263b4cad8490db957beaf5df9b519bf
theprimefibber <dev@thisispalash.com>
2026-09-09 11:48:57 +0530

    dictionary contracts done

--- contracts/src/dictionary/IkhaaliDictionaryV2.sol
+++ contracts/src/dictionary/IkhaaliDictionaryV2.sol
@@ -3,15 +3,34 @@ pragma solidity ^0.8.20;
 
 import {IkhaaliDeprecationV1} from "../util/IkhaaliDeprecationV1.sol";
 
-/// @title IkhaaliDictionaryV1
+struct khaaliDictionary {
+  address LOCATION; /// @dev we use SSTORE2 to store the dictionary data
+  uint64 WORD_COUNT;
+  uint32 WORD_LENGTH; /// @dev length of each word in bytes (padded)
+  string NAME;
+}
+
+/// @title IkhaaliDictionaryV2
 /// @notice Interface for onchain word dictionaries used by khaaliNames
+/// @dev Consider multi pointer dictionaries for V3
 interface IkhaaliDictionaryV2 is IkhaaliDeprecationV1 {
+
+  ////////// Errors //////////
+
+  error DictionaryNeedsName();
+  error WordLengthMustNotBeZero();
+  error DictionaryMustNotBeEmpty();
+  error DictionaryMustNotBeZeroAddress();
+  error DictionaryLocationBlobSizeInvalid(uint256 expected, uint256 received);
 
   error DictionaryIndexOutOfBounds (
     address dictionary,
     uint256 length,
     uint256 index
   );
+
+
+  ////////// Public Functions //////////
 
   /// @notice Return the word at a certain index
   /// @param index The zero-based index in the dictionary
@@ -23,4 +42,23 @@ interface IkhaaliDictionaryV2 is IkhaaliDeprecationV1 {
 
   /// @return The max byte length for each word
   function wordLength() external view returns (uint256);
+
+
+  ////////// Admin Functions //////////
+
+  /// @notice Identical to `wordAt` with the exception of being an escape hatch
+  ///         for the admin post deprecation
+  /// @dev Intended use is as seeds for new version
+  /// @param index The zero-based index in the dictionary
+  /// @return The word as a string
+  function migrate(uint256 index) external view returns (string memory);
 }
+
+/** @dev V3 considerations ~
+ *    . Smarter fingerprinting
+ *    . Stronger constructor checks for data contract before assignment
+ *    . Batch reads, tho unsure if and where needed
+ *    . Dictionaries larger than 24KB (ie, multi pointer)
+ *    . Registry contract to decouple data with logic
+ *    . Reverse lookup, O(n) is a concern
+ */
--- contracts/src/dictionary/khaaliDictionaryV2.sol
+++ contracts/src/dictionary/khaaliDictionaryV2.sol
@@ -1,20 +1,124 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.20;
 
-import {IkhaaliDictionaryV2} from "./IkhaaliDictionaryV2.sol";
-import {khaaliDeprecationV1} from "../util/khaaliDeprecationV1.sol";
 import {SSTORE2} from "solady/utils/SSTORE2.sol";
+import {khaaliDeprecationV1} from "../util/khaaliDeprecationV1.sol";
+import {IkhaaliDictionaryV2, khaaliDictionary} from "./IkhaaliDictionaryV2.sol";
 
-contract khaaliDictionaryV2 is khaaliDeprecationV1 {
+contract khaaliDictionaryV2 is IkhaaliDictionaryV2, khaaliDeprecationV1 {
 
-  string public name;
+  khaaliDictionary public dict;
+  bytes32 public immutable FINGERPRINT;
+
+
+  ////////// Initializers //////////
 
   constructor(
     uint64 _major, uint64 _minor, uint128 _patch,
-    string memory _name
+    uint32 _length, uint32 _count, address _loc, string memory _name
   )
     khaaliDeprecationV1(_major, _minor, _patch)
   {
-    name = _name;
+    require(_length != 0, WordLengthMustNotBeZero());
+    require(_count != 0, DictionaryMustNotBeEmpty());
+    require(bytes(_name).length > 0, DictionaryNeedsName());
+    require(_loc != address(0), DictionaryMustNotBeZeroAddress());
+    require(
+      _loc.code.length == (uint256(_count) * _length) + 10,
+      DictionaryLocationBlobSizeInvalid(
+        (uint256(_length) * _count) + 10, _loc.code.length
+      )
+    );
+
+    /// @dev further hardening for SSTORE2 deployment may be useful in V3
+
+    dict = khaaliDictionary({
+      LOCATION: _loc,
+      WORD_COUNT: _count,
+      WORD_LENGTH: _length,
+      NAME: _name
+    });
+    FINGERPRINT = keccak256(SSTORE2.read(_loc, 0, (uint256(_count) * _length)));
   }
+
+
+  ////////// Public Functions //////////
+
+  function wordLength()
+    public
+    view
+    undeprecated
+    returns (uint256)
+  {
+    return dict.WORD_LENGTH;
+  }
+
+  function wordCount()
+    public
+    view
+    undeprecated
+    returns (uint256)
+  {
+    return dict.WORD_COUNT;
+  }
+
+  function wordAt(uint256 _i)
+    public
+    view
+    undeprecated
+    returns (string memory)
+  {
+    return _get(_i);
+  }
+
+
+  ////////// Admin Functions //////////
+
+  function migrate(uint256 _i)
+    public
+    view
+    onlyAdmin /// @dev notice the modifer switch for "escape hatch"
+    returns (string memory)
+  {
+    return _get(_i);
+  }
+
+
+  ////////// Internal Functions //////////
+
+  /// @dev Not rule-of-3, but 2 identicals also annoy me
+  function _get(uint256 _i) internal view returns (string memory) {
+    require(
+      _i < dict.WORD_COUNT,
+      DictionaryIndexOutOfBounds(address(this), dict.WORD_COUNT, _i)
+    );
+    bytes memory _raw = SSTORE2.read(
+      dict.LOCATION, _i * dict.WORD_LENGTH, (_i + 1) * dict.WORD_LENGTH
+    );
+    return _bytes2str(_raw);
+  }
+
+  /// @notice Convert the raw bytes to a string, trimming the padding bytes
+  /// @notice AI generated logic, lgtm; tho, "trust, but verify"!
+  /// @dev 1 loop is cheaper than 2 loops by 100-150 gas
+  function _bytes2str(bytes memory _raw)
+    internal
+    view
+    returns (string memory)
+  {
+    uint256 _len;
+    bytes memory _word = new bytes(dict.WORD_LENGTH);
+
+    for (_len = 0; _len < dict.WORD_LENGTH; _len++) {
+      if (_raw[_len] == 0) break;
+      _word[_len] = _raw[_len];
+    }
+
+    assembly {
+      mstore(_word, _len) // overwrite the length of the word
+    }
+
+    return string(_word);
+  }
+
 }
